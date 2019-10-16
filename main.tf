c835fab13bb1b39cb19bb624ed423aa0e6da86d0
gmohana <53671570+gmohana@users.noreply.github.com>
2019-10-16 21:08:31 +0530

    create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws"{
+  # Credentials to access aws cluster
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "${var.region}"
+}
+
+resource "aws_instance" "test-inst" {
+  instance_type = "${var.instance_type}"
+  ami           = "${var.ami}"
+  count         = 1
+# vpc_id        = "${var.vpcid}" # Not required, subnet id will pick vpc_id
+  subnet_id     = "${var.subnet_id}"
+  associate_public_ip_address = TRUE
