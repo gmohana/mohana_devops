b4712c8d9343baf1c4b2102b1826285347d10aff
gmohana <53671570+gmohana@users.noreply.github.com>
2019-10-16 21:10:24 +0530

    create variable.tf

---
+++ vvariable.tf
@@ -0,0 +1,8 @@
+variable access_key {}
+variable secret_key {}
+variable region {}
+variable instance_type {}
+variable ami {}
+variable subnet_id {}
+variable vpc_id {}
+variable security_group_id {}
